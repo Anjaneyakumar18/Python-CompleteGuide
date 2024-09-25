ed2edf16912354b903aa3dcf6bc21d428a9816c0
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-25 19:17:56 +0530

    Create make use of module.py

---
+++ 6.Modules/2.Custom modules/make use of module.py
@@ -0,0 +1,11 @@
+# to use functions and data
+import creating_module as o
+o.name()
+o.age()
+print(o.height)
+#to use class methods and class data
+from creating_module import student
+
+
+s=student()
+student.college()
