6494a7f5adba3f3bee1a8b2626e9347591ae9a09
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-25 19:12:35 +0530

    Create creating module.py

---
+++ 6.Modules/2.Custom modules/creating module.py
@@ -0,0 +1,18 @@
+###data
+
+Name="Anjaneya Kumar"
+
+Age="20 Years"
+
+height=1.73
+
+
+# methods
+def name():
+  print("I am Anjaneya Kumar Ramisetty!")
+
+def age():
+  print("I am 20 years old!")
+
+def skills():
+  print("I am skilled in python,DSA,Web devlopment,SQL and Java")
