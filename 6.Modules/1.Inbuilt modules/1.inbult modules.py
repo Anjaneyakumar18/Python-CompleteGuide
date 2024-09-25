3ddc89f6b9c0874dafaed8120d50b6633f51f30e
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-25 19:03:31 +0530

    Create 1.inbult modules.py

---
+++ 6.Modules/1.Inbuilt modules/1.inbult modules.py
@@ -0,0 +1,16 @@
+import math
+
+# Using math functions
+print("Square root of 16:", math.sqrt(16))         # Square root
+print("Value of π:", math.pi)                        # Value of π
+print("Cosine of 0 radians:", math.cos(0))          # Cosine
+
+
+
+
+import random
+
+# Generating random numbers
+print("Random integer between 1 and 10:", random.randint(1, 10))
+print("Random float between 0 and 1:", random.random())
+print("Random choice from a list:", random.choice(['apple', 'banana', 'cherry']))
