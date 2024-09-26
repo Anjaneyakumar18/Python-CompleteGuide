d2c3071b969eb968946f3c45eb8c48492dc78d5e
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 19:44:55 +0530

    Create 4.string.py

---
+++ 1.Data Types/4.string.py
@@ -0,0 +1,28 @@
+s=str()  #string class constructor
+print(type(s)) #<class "str">
+
+s=""  #string data type
+
+s="String data type"
+
+print(s) #string data type
+
+print(len(s))  #16
+
+
+'''there are many inbuilt string functions in python'''
+
+
+'''
+
+"hello".upper()  # "HELLO"
+"HELLO".lower()  # "hello"
+"hello world".capitalize()  # "Hello world"
+"hello world".title()  # "Hello World"
+"   hello   ".strip()  # "hello"
+"hello world".split()  # ['hello', 'world']
+", ".join(["apple", "banana", "cherry"])  # "apple, banana, cherry"
+"hello".find("e")  # 1
+"hello".startswith("he")  # True
+
+'''
