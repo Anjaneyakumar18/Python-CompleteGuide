4f47fbe4f557814c499c13866f61faad9f799e4b
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 19:30:25 +0530

    Create 2.float.py

---
+++ 1.Data Types/2.float.py
@@ -0,0 +1,26 @@
+# Defining float variables
+num1 = 10.5
+num2 = 2.0
+
+# Performing basic operations
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+# Printing results
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+
+
+
+# Converting integer to float
+int_num = 5
+float_num = float(int_num)
+
+# Printing the converted value
+print("Integer:", int_num, "as Float:", float_num)
+
+
