d6ffdba8a5d3d5efd74a251ef0666288d8d21df1
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 19:19:26 +0530

    Create while loop in java .java

---
+++ 4.Looping statements/while loop in java .java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // Initialize the counter
+        
+        // While loop to iterate until i is less than or equal to 10
+        while (i <= 10) {
+            System.out.println("Number: " + i);
+            i++; // Increment the counter
+        }
+    }
+}
