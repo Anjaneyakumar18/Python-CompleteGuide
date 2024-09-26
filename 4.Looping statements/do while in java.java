65cb4a7df71fc945278dbef48172bda2a8cf5501
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 19:20:09 +0530

    Create do while in java.java

---
+++ 4.Looping statements/do while in java.java
@@ -0,0 +1,11 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // Initialize the counter
+        
+        // Do-while loop to iterate until i is greater than 10
+        do {
+            System.out.println("Number: " + i);
+            i++; // Increment the counter
+        } while (i <= 10);
+    }
+}
