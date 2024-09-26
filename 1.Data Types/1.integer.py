bd9a2853ea59d2e7e218dfa981b6a49bcea883f2
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 19:28:49 +0530

    Create 1.integer.py

---
+++ 1.Data Types/1.integer.py
@@ -0,0 +1,24 @@
+a=10 #assiging a with the value 10
+
+print(type(a))  # <class 'int'>
+
+b=10+10   # we can even assign expression also
+print(b)  #output b  type=int
+
+c=10*10
+
+print(c)  # 100 integer type
+
+c=10/5  # division has floor returns
+
+print(c)  # 2.0  
+
+print(type(c)) #<class "float">
+
+
+d=10//2  # floor division has int value
+
+print(d) #5  type=int
+
+
+
