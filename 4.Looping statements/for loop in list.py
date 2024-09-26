eb8eff10322e8bf1ef302c831ddb78c726f9686d
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 18:50:01 +0530

    Create for loop in list.py

---
+++ 4.Looping statements/for loop in list.py
@@ -0,0 +1,30 @@
+lst=[1,2,3,4,5,6]
+
+for num in lst:
+  print(num)
+
+"""output:
+1
+2
+3
+4
+5
+6
+"""
+
+
+for i in range(len(lst)):
+  print(lst[i])
+
+"""
+
+output::
+
+1
+2
+3
+4
+5
+6
+
+"""
