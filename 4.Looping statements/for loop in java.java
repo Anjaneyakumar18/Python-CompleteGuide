1276c2bcf09bb236e359d82e6955433330a11993
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-09-26 19:18:51 +0530

    Create for loop in java.java

---
+++ 4.Looping statements/for loop in java.java
@@ -0,0 +1,8 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // For loop to iterate from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
