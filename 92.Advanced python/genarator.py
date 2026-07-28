203546d35dec9ef0593ca74cbd161f7ce6df95cf
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2026-07-28 14:27:49 +0530

    Correct function name and enhance generator comments
    Fixed typo in function name and improved comments.

--- 92.Advanced python/genarator.py
+++ 92.Advanced python/genarator.py
@@ -1,7 +1,11 @@
+'''A generator is an object that produces values one at a time instead of creating all values at once.
+
+Instead of storing every value in memory, it generates values only when needed.'''
+
 def genarte(n):
     i=0
     while i<n:
-        yield i
+        yield i   ''' Yield vs return is a famous question where retrun actually returns the method while a yield return and save where the execution has stoped'''
         i+=1
 gen=genarte(5)
 for i in range(4):
